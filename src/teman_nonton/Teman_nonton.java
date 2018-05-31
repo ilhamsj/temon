d805813b2421e974c19c14442f5f7353b7a63f09
ilhamsj <ilhamsaputrajati@gmail.com>
2018-05-31 12:18:47 +0700

    start building

---
+++ src/teman_nonton/Teman_nonton.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package teman_nonton;
+
+/**
+ *
+ * @author ilham
+ */
+public class Teman_nonton {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
